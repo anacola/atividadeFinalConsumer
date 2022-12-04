8f7b693e6e75d74cbd69f7b7d0123b34b2256624
Ana Cola <54247246+anacola@users.noreply.github.com>
2022-12-04 20:00:20 -0300

    Update QueueConsumer.java
    inserção do container factory

--- src/main/java/br/com/fiap/scjr/atividadeFinalConsumer/consumer/QueueConsumer.java
+++ src/main/java/br/com/fiap/scjr/atividadeFinalConsumer/consumer/QueueConsumer.java
@@ -6,14 +6,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import com.google.gson.Gson;
 
+import java.util.List;
+
+
 @Component
 public class QueueConsumer {
+
+    @Value("${spring.mail.username}")
+    private String destinatarioEmail;
+    @Value("${spring.mail.username}")
+    private String remetenteEmail;
 
     @Autowired
     private JavaMailSender mailSender; //Para pegar as configuracoes no application.yml
@@ -21,15 +30,16 @@ public class QueueConsumer {
     private DroneValidation droneValidator = new DroneValidation();//Cria uma instancia para da classe de validacao
     private Gson gson = new Gson();//Cria uma instancia do GSON para converter as mensagens para objetos java
 
-    @RabbitListener(queues = {"${queue.fiap}"})
-    public void receiveMessage(@Payload String fileBody) {
+    @RabbitListener(queues = {"${queue.fiap.nome}"}, containerFactory = "consumerBatchContainerFactory")
+    public void receiveMessage(@Payload List<String> messages) {
         try {
-            DroneModel drone = gson.fromJson(fileBody, DroneModel.class);//Converte a mensagen de Json para objeto java
+            for (String message: messages) {
+                DroneModel drone = gson.fromJson(message, DroneModel.class);//Converte a mensagen de Json para objeto java
                 if (!droneValidator.validar(drone)) {//Valida se ha alguma temperatura ou umidade inconsistente, se estiver inconsistente, envia email
                     log.info(sendMailDrone(drone));
                 }
-
-            Thread.sleep(60000);//Trava a thread por 1min, para depois disso executar ela novamente. Tentei usar o Scheduler do Springboot, mas nao consegui :'(
+            }
+            Thread.sleep(60000);//Trava a thread por 1min, para depois disso executar ela novamente.
         } catch (InterruptedException e) {
             log.info("Problemas na thread: " + e.getMessage());
         }
@@ -42,8 +52,8 @@ public class QueueConsumer {
                 "Seu drone com o número de identificação: " +drone.getIdDrone() + ", obteve dados anormais:\n" +
                 "Temperatura: " + drone.getTemperatura() + ", Umidade: " + drone.getUmidade() + ", Latitude: " + drone.getLatitude()
                 + ", Longitude: " + drone.getLongitude());
-        message.setTo("inserirseuemail@hotmail.com");
-        message.setFrom("Drone Information  <inserirseuemail@hotmail.com>");
+        message.setTo(destinatarioEmail);
+        message.setFrom(remetenteEmail);
         try {
             mailSender.send(message);
             return "Email enviado com sucesso!";
@@ -53,4 +63,3 @@ public class QueueConsumer {
         }
     }
 }
-
